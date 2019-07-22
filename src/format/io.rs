8c835993478d8b3b14d90c1bdb56880e522a681e
Ondrej Perutka <ondrej.perutka@angelcam.com>
2019-07-22 14:46:39 +0200

    Extend the format::io module with IO reader and make the reader and the writer compatible with anything that implements std::io::Read/Write

--- src/format/io.rs
+++ src/format/io.rs
@@ -1,6 +1,9 @@
+use std::io;
 use std::slice;
 
-use bytes::BytesMut;
+use std::io::{Read, Write};
+
+use bytes::{Bytes, BytesMut};
 
 use libc::{c_int, c_void, int64_t, uint8_t};
 
@@ -19,8 +22,20 @@ extern "C" {
         write_packet: Option<WritePacket>,
         seek: Option<Seek>,
     ) -> *mut c_void;
-
     fn ffw_io_context_free(context: *mut c_void);
+    fn ffw_io_error_eof() -> c_int;
+    fn ffw_io_error_would_block() -> c_int;
+    fn ffw_io_error_unknown() -> c_int;
+    fn ffw_io_error_posix(error: c_int) -> c_int;
+}
+
+/// Common trait for readable IO contexts.
+pub trait Reader {
+    /// Get a pointer to the underlying AVIOContext.
+    fn as_ptr(&self) -> *const c_void;
+
+    /// Get a mut pointer to the underlying AVIOContext.
+    fn as_mut_ptr(&mut self) -> *mut c_void;
 }
 
 /// Common trait for writable IO contexts.
@@ -39,7 +54,7 @@ pub struct IOContext {
 
 impl IOContext {
     /// Create a new IO context from its raw representation.
-    pub unsafe fn from_raw_ptr(ptr: *mut c_void) -> IOContext {
+    pub unsafe fn from_raw_ptr(ptr: *mut c_void) -> Self {
         IOContext { ptr: ptr }
     }
 
@@ -63,44 +78,82 @@ impl Drop for IOContext {
 unsafe impl Send for IOContext {}
 unsafe impl Sync for IOContext {}
 
-/// A WritePacket function for the MemWriter.
-extern "C" fn mem_writer_write_packet(
+/// A ReadPacket function for the IOReader.
+extern "C" fn io_reader_read_packet<T>(
     opaque: *mut c_void,
     buffer: *mut uint8_t,
     buffer_size: c_int,
-) -> c_int {
-    let output_ptr = opaque as *mut BytesMut;
+) -> c_int
+where
+    T: Read,
+{
+    let input_ptr = opaque as *mut T;
 
-    let output = unsafe { &mut *output_ptr };
+    let input = unsafe { &mut *input_ptr };
 
-    let data = unsafe { slice::from_raw_parts(buffer as *mut u8, buffer_size as usize) };
+    let buffer = unsafe { slice::from_raw_parts_mut(buffer as *mut u8, buffer_size as usize) };
 
-    output.extend_from_slice(data);
-
-    buffer_size
+    match input.read(buffer) {
+        Ok(n) => {
+            if n > 0 {
+                n as c_int
+            } else {
+                unsafe { ffw_io_error_eof() }
+            }
+        }
+        Err(err) => {
+            if let Some(code) = err.raw_os_error() {
+                unsafe { ffw_io_error_posix(code as _) }
+            } else if err.kind() == io::ErrorKind::WouldBlock {
+                unsafe { ffw_io_error_would_block() }
+            } else {
+                unsafe { ffw_io_error_unknown() }
+            }
+        }
+    }
 }
 
-/// An AVIO writer that puts everything into memory.
-pub struct MemWriter {
+/// An AVIO reader that reads data from a given input.
+pub struct IOReader<T> {
     io_context: IOContext,
-    output: Box<BytesMut>,
+    input: Box<T>,
 }
 
-impl MemWriter {
-    /// Create a new writer.
-    pub fn new() -> MemWriter {
-        let mut output = Box::new(BytesMut::new());
+impl<T> IOReader<T>
+where
+    T: Read,
+{
+    /// Create a new AVIO reader from a given input.
+    pub fn new(input: T) -> Self {
+        Self::from(Box::new(input))
+    }
 
-        let output_ptr = output.as_mut() as *mut BytesMut;
-        let opaque_ptr = output_ptr as *mut c_void;
+    /// Get reference to the underlying input.
+    pub fn input(&self) -> &T {
+        self.input.as_ref()
+    }
+
+    /// Get mutable reference to the underlying input.
+    pub fn input_mut(&mut self) -> &mut T {
+        self.input.as_mut()
+    }
+}
+
+impl<T> From<Box<T>> for IOReader<T>
+where
+    T: Read,
+{
+    fn from(mut input: Box<T>) -> Self {
+        let input_ptr = input.as_mut() as *mut T;
+        let opaque_ptr = input_ptr as *mut c_void;
 
         let io_context = unsafe {
             ffw_io_context_new(
                 4096,
-                1,
+                0,
                 opaque_ptr,
+                Some(io_reader_read_packet::<T>),
                 None,
-                Some(mem_writer_write_packet),
                 None,
             )
         };
@@ -111,24 +164,331 @@ impl MemWriter {
 
         let io_context = unsafe { IOContext::from_raw_ptr(io_context) };
 
-        MemWriter {
-            io_context: io_context,
-            output: output,
-        }
-    }
-
-    /// Take the currently buffered output data
-    pub fn take_data(&mut self) -> BytesMut {
-        self.output.take()
+        Self { io_context, input }
     }
 }
 
-impl Writer for MemWriter {
+impl<T> Reader for IOReader<T> {
     fn as_ptr(&self) -> *const c_void {
         self.io_context.as_ptr()
     }
 
     fn as_mut_ptr(&mut self) -> *mut c_void {
         self.io_context.as_mut_ptr()
+    }
+}
+
+/// Helper struct.
+struct BytesReader {
+    data: Bytes,
+    closed: bool,
+}
+
+impl BytesReader {
+    /// Push more data to the reader.
+    fn push_data<T>(&mut self, data: T)
+    where
+        T: AsRef<[u8]>,
+    {
+        if self.closed {
+            panic!("unable to push more data, the input has been already closed");
+        }
+
+        self.data.extend_from_slice(data.as_ref());
+    }
+
+    /// Close the reader.
+    fn close(&mut self) {
+        self.closed = true;
+    }
+}
+
+impl Default for BytesReader {
+    fn default() -> Self {
+        Self::from(Bytes::new())
+    }
+}
+
+impl From<Bytes> for BytesReader {
+    fn from(data: Bytes) -> Self {
+        Self {
+            data,
+            closed: false,
+        }
+    }
+}
+
+impl Read for BytesReader {
+    fn read(&mut self, buffer: &mut [u8]) -> Result<usize, io::Error> {
+        if self.data.is_empty() {
+            if self.closed {
+                return Ok(0);
+            } else {
+                return Err(io::Error::from(io::ErrorKind::WouldBlock));
+            }
+        }
+
+        let available = self.data.len();
+        let capacity = buffer.len();
+
+        let take = available.min(capacity);
+
+        let data = self.data.split_to(take);
+        let buffer = &mut buffer[..take];
+
+        buffer.copy_from_slice(data.as_ref());
+
+        Ok(take)
+    }
+}
+
+/// An AVIO reader that takes everything from memory.
+pub struct MemReader {
+    inner: IOReader<BytesReader>,
+}
+
+impl MemReader {
+    /// Create a new memory reader for given data.
+    pub fn new<T>(data: T) -> Self
+    where
+        T: AsRef<[u8]>,
+    {
+        Self::from(Bytes::from(data.as_ref()))
+    }
+}
+
+impl From<Bytes> for MemReader {
+    fn from(data: Bytes) -> Self {
+        let mut reader = BytesReader::from(data);
+
+        reader.close();
+
+        Self {
+            inner: IOReader::new(reader),
+        }
+    }
+}
+
+impl Reader for MemReader {
+    fn as_ptr(&self) -> *const c_void {
+        self.inner.as_ptr()
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut c_void {
+        self.inner.as_mut_ptr()
+    }
+}
+
+/// An AVIO reader that takes everything from memory. This reader allows asynchronous operation.
+/// You can push more data (if available). You also need to call the close method in order to tell
+/// the consumer that there will be no more data.
+pub struct AsyncMemReader {
+    inner: IOReader<BytesReader>,
+}
+
+impl AsyncMemReader {
+    /// Push more data to the input.
+    pub fn push_data<T>(&mut self, data: T)
+    where
+        T: AsRef<[u8]>,
+    {
+        self.inner.input_mut().push_data(data)
+    }
+
+    /// Close the input.
+    pub fn close(&mut self) {
+        self.inner.input_mut().close()
+    }
+}
+
+impl Default for AsyncMemReader {
+    fn default() -> Self {
+        Self {
+            inner: IOReader::new(BytesReader::default()),
+        }
+    }
+}
+
+impl Reader for AsyncMemReader {
+    fn as_ptr(&self) -> *const c_void {
+        self.inner.as_ptr()
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut c_void {
+        self.inner.as_mut_ptr()
+    }
+}
+
+/// A WritePacket function for the IOWriter.
+extern "C" fn io_writer_write_packet<T>(
+    opaque: *mut c_void,
+    buffer: *mut uint8_t,
+    buffer_size: c_int,
+) -> c_int
+where
+    T: Write,
+{
+    let output_ptr = opaque as *mut T;
+
+    let output = unsafe { &mut *output_ptr };
+
+    if !buffer.is_null() && buffer_size > 0 {
+        let buffer = unsafe { slice::from_raw_parts(buffer as *mut u8, buffer_size as usize) };
+
+        match output.write(buffer) {
+            Ok(n) => {
+                if n > 0 {
+                    n as c_int
+                } else {
+                    unsafe { ffw_io_error_eof() }
+                }
+            }
+            Err(err) => {
+                if let Some(code) = err.raw_os_error() {
+                    unsafe { ffw_io_error_posix(code as _) }
+                } else if err.kind() == io::ErrorKind::WouldBlock {
+                    unsafe { ffw_io_error_would_block() }
+                } else {
+                    unsafe { ffw_io_error_unknown() }
+                }
+            }
+        }
+    } else if let Err(err) = output.flush() {
+        if let Some(code) = err.raw_os_error() {
+            unsafe { ffw_io_error_posix(code) }
+        } else if err.kind() == io::ErrorKind::WouldBlock {
+            unsafe { ffw_io_error_would_block() }
+        } else {
+            unsafe { ffw_io_error_unknown() }
+        }
+    } else {
+        0
+    }
+}
+
+/// An AVIO writer that writes data into a given output.
+pub struct IOWriter<T> {
+    io_context: IOContext,
+    output: Box<T>,
+}
+
+impl<T> IOWriter<T>
+where
+    T: Write,
+{
+    /// Create a new AVIO writer from a given output.
+    pub fn new(output: T) -> Self {
+        Self::from(Box::new(output))
+    }
+
+    /// Get reference to the underlying output.
+    pub fn output(&self) -> &T {
+        self.output.as_ref()
+    }
+
+    /// Get mutable reference to the underlying output.
+    pub fn output_mut(&mut self) -> &mut T {
+        self.output.as_mut()
+    }
+}
+
+impl<T> From<Box<T>> for IOWriter<T>
+where
+    T: Write,
+{
+    fn from(mut output: Box<T>) -> Self {
+        let output_ptr = output.as_mut() as *mut T;
+        let opaque_ptr = output_ptr as *mut c_void;
+
+        let io_context = unsafe {
+            ffw_io_context_new(
+                4096,
+                1,
+                opaque_ptr,
+                None,
+                Some(io_writer_write_packet::<T>),
+                None,
+            )
+        };
+
+        if io_context.is_null() {
+            panic!("unable to allocate an AVIO context");
+        }
+
+        let io_context = unsafe { IOContext::from_raw_ptr(io_context) };
+
+        Self { io_context, output }
+    }
+}
+
+impl<T> Writer for IOWriter<T> {
+    fn as_ptr(&self) -> *const c_void {
+        self.io_context.as_ptr()
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut c_void {
+        self.io_context.as_mut_ptr()
+    }
+}
+
+/// Helper struct.
+struct BytesMutWriter {
+    inner: BytesMut,
+}
+
+impl BytesMutWriter {
+    /// Take the currently buffered data.
+    fn take_data(&mut self) -> BytesMut {
+        self.inner.take()
+    }
+}
+
+impl Default for BytesMutWriter {
+    fn default() -> Self {
+        Self {
+            inner: BytesMut::new(),
+        }
+    }
+}
+
+impl Write for BytesMutWriter {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        self.inner.extend_from_slice(buf);
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        Ok(())
+    }
+}
+
+/// An AVIO writer that puts everything into memory.
+pub struct MemWriter {
+    inner: IOWriter<BytesMutWriter>,
+}
+
+impl MemWriter {
+    /// Take the currently buffered output data.
+    pub fn take_data(&mut self) -> BytesMut {
+        self.inner.output_mut().take_data()
+    }
+}
+
+impl Default for MemWriter {
+    fn default() -> Self {
+        Self {
+            inner: IOWriter::new(BytesMutWriter::default()),
+        }
+    }
+}
+
+impl Writer for MemWriter {
+    fn as_ptr(&self) -> *const c_void {
+        self.inner.as_ptr()
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut c_void {
+        self.inner.as_mut_ptr()
     }
 }
