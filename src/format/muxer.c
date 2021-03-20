97d8da7ac99ad71f16003fe212a07f5de8a67a7e
Thomas Nibler <dev@tnibler.de>
2021-03-20 22:27:10 +0100

    Allow setting stream metadata from safe rust

--- src/format/muxer.c
+++ src/format/muxer.c
@@ -21,6 +21,8 @@ typedef struct Muxer {
 Muxer* ffw_muxer_new();
 unsigned ffw_muxer_get_nb_streams(const Muxer*);
 int ffw_muxer_new_stream(Muxer*, const AVCodecParameters*);
+int ffw_muxer_set_int_option(Muxer* const, int, const char*, int);
+int ffw_muxer_set_string_option(Muxer* const, int, const char*, const char*);
 int ffw_muxer_init(Muxer*, AVIOContext*, AVOutputFormat*);
 int ffw_muxer_get_option(Muxer*, const char*, uint8_t**);
 int ffw_muxer_set_initial_option(Muxer*, const char*, const char*);
@@ -73,6 +75,16 @@ int ffw_muxer_new_stream(Muxer* muxer, const AVCodecParameters* params) {
     s->codecpar->codec_tag = 0;
 
     return s->index;
+}
+
+int ffw_muxer_set_int_option(Muxer* const muxer, int stream_index, const char* key, int value) {
+    AVStream* stream = muxer->fc->streams[stream_index];
+    return av_dict_set_int(&stream->metadata, key, value, 0);
+}
+
+int ffw_muxer_set_string_option(Muxer* const muxer, int stream_index, const char* key, const char* value) {
+    AVStream* stream = muxer->fc->streams[stream_index];
+    return av_dict_set(&stream->metadata, key, value, 0);
 }
 
 int ffw_muxer_init(
