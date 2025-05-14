bedb6ba0ad29d7eee81f34d2b392430e408dad32
Ben Duffield <jebavarde@gmail.com>
2025-05-14 07:53:49 -0400

    Fix seeking - handle `whence` properly (#73)
    ---------
    
    Co-authored-by: Benjamin Duffield <bduffield@palantir.com>

--- ac-ffmpeg/src/format/io.c
+++ ac-ffmpeg/src/format/io.c
@@ -11,8 +11,17 @@ typedef int write_packet_t(void*, const uint8_t*, int);
 
 typedef int64_t seek_t(void*, int64_t, int);
 
-int ffw_io_is_avseek_size(int whence) {
-    return whence & AVSEEK_SIZE;
+int ffw_io_whence_to_seek_mode(int whence) {
+    if (whence & AVSEEK_SIZE) {
+        return 0;
+    }
+
+    switch (whence) {
+        case SEEK_SET: return 1;
+        case SEEK_CUR: return 2;
+        case SEEK_END: return 3;
+        default: return -1;
+    }
 }
 
 AVIOContext * ffw_io_context_new(
